4e93be7078e1e2d3dd4f12df2522a8c6a266b519
juugeebee <july2023@yahoo.fr>
2019-11-11 14:15:40 +0100

    generation liste de dico mRNA fasta

--- mRNA_to_ORF.py
+++ mRNA_to_ORF.py
@@ -3,3 +3,30 @@
 Master 2 BIB Paris Diderot - 2019-2020
 Projet Long
 """
+
+#Import des modules
+import os
+from Bio import SeqIO
+
+def listdir_nohidden(path):
+    for f in os.listdir(path):
+        if not f.startswith('.'):
+            yield f
+
+
+## MAIN ##
+fasta_list = listdir_nohidden('./mRNA_fasta')
+
+#récupération séquences sous forme de dictionnaire - clefs = ids -
+path = './mRNA_fasta/'
+dico={}
+for file_name in fasta_list:
+    gene_name = (file_name.split('_')[0])
+    dico[gene_name+'_mRNA'] = []
+    dico[gene_name+'_mRNA'].append(SeqIO.to_dict(SeqIO.parse(path+file_name, 'fasta')))
+
+for k in dico.keys():
+    print(k)
+
+for v in dico.values():
+        print(v)
