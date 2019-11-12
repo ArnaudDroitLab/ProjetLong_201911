0bec65e0ad03a61193d5a417e3e92063298aa82c
juugeebee <july2023@yahoo.fr>
2019-11-12 13:37:02 +0100

    dataframe mRNA_sequences

--- mRNA_to_ORF.py
+++ mRNA_to_ORF.py
@@ -6,6 +6,7 @@ Projet Long
 
 #Import des modules
 import os
+import pandas
 from Bio import SeqIO
 
 def listdir_nohidden(path):
@@ -21,11 +22,33 @@ fasta_list = listdir_nohidden('./mRNA_fasta')
 #récupération séquences sous forme de dictionnaire - clefs = ids -
 path = './mRNA_fasta/'
 dico={}
+mRNA_sequence = []
+start = []
+stop = []
+gene = []
+
 for file_name in fasta_list:
     gene_name = (file_name.split('_')[0])
     dico[gene_name+'_mRNA'] = []
     dico[gene_name+'_mRNA'].append(SeqIO.to_dict(SeqIO.parse(path+file_name, 'fasta')))
+    
     for id in dico[gene_name+'_mRNA']:
         for value in id.values():
+            stop_list = []
             mydna = value.seq
-            print(mydna.find('ATG'))
+            mRNA_sequence.append(str(value.seq))
+            start.append(mydna.find('ATG'))
+            stop_list.append(mydna.find('TGA'))
+            stop_list.append(mydna.find('TAG'))
+            stop_list.append(mydna.find('TAA'))   
+            stop.append(stop_list)
+            gene.append(gene_name)
+
+
+df = pandas.DataFrame(columns = ['gene', 'mRNA_sequence','start','stop','ORF_sequence'])
+df['gene'] = pandas.Series(gene)
+df['mRNA_sequence'] = pandas.Series(mRNA_sequence)
+df['start'] = pandas.Series(start)
+df['stop'] = pandas.Series(stop)
+
+print(df)
