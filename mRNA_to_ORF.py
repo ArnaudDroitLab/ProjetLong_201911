bc8740082f375ca97fafae635daaeda42e842d34
juugeebee <july2023@yahoo.fr>
2019-11-19 16:40:03 +0100

    seaparation sens/antisens, transcrits/mutations

--- mRNA_to_ORF.py
+++ mRNA_to_ORF.py
@@ -7,6 +7,7 @@ Projet Long
 #Import des modules
 import os
 import pandas
+import re
 from Bio import SeqIO
 
 
@@ -22,12 +23,16 @@ def listdir_nohidden(path):
 fasta_list = listdir_nohidden('./mRNA_fasta')
 path = './mRNA_fasta/'
 dico={}
-mRNA_name = []
-mRNA_sequence = []
+transcript_name = []
+mutation_name = []
+mRNA_sens = []
+mRNA_antisens = []
 gene = []
 longueur_mRNA = []
-nombre_ORF = []
-orf = []
+nombre_ORF_sens = []
+nombre_ORF_antisens = []
+ORF_sens = []
+ORF_antisens = []
 
 for file_name in fasta_list:
     gene_name = (file_name.split('_')[0])
@@ -36,11 +41,15 @@ for file_name in fasta_list:
     
     for id in dico[gene_name+'_mRNA']:
         for key in id.keys():
-            mRNA_name.append(key)
+            mRNA_name = re.sub('line(\d)+','',key)
+            transcript_name.append(mRNA_name[0:9])
+            mutation_name.append(mRNA_name[9:])
         
         for value in id.values():
-            orf_list = []
-            mRNA_sequence.append(str(value.seq))
+            orf_sens_list = []
+            orf_antisens_list = []
+            mRNA_sens.append(str(value.seq))
+            mRNA_antisens.append(str(value.seq.complement()))
             longueur_mRNA.append(len(str(value.seq)))
             gene.append(gene_name)
             
@@ -56,11 +65,11 @@ for file_name in fasta_list:
                 orf_TAG_sens = sens[ATG_indice_sens:TAG_indice_sens]
                 orf_TAA_sens = sens[ATG_indice_sens:TAA_indice_sens]
                 if len(str(orf_TGA_sens)) >= 30 and len(str(orf_TGA_sens))%3==0:
-                    orf_list.append(str(orf_TGA_sens))
+                    orf_sens_list.append(str(orf_TGA_sens))
                 if len(str(orf_TAG_sens)) >= 30 and len(str(orf_TAG_sens))%3==0:
-                    orf_list.append(str(orf_TAG_sens))
+                    orf_sens_list.append(str(orf_TAG_sens))
                 if len(str(orf_TAA_sens)) >= 30 and len(str(orf_TAA_sens))%3==0:
-                    orf_list.append(str(orf_TAA_sens))
+                    orf_sens_list.append(str(orf_TAA_sens))
             
                 #Antisens
                 antisens = sens.complement()
@@ -72,24 +81,30 @@ for file_name in fasta_list:
                 orf_TAG_antisens = antisens[ATG_indice_antisens:TAG_indice_antisens]
                 orf_TAA_antisens = antisens[ATG_indice_antisens:TAA_indice_antisens]
                 if len(str(orf_TGA_antisens)) >= 30 and len(str(orf_TGA_antisens))%3==0 :
-                    orf_list.append(str(orf_TGA_antisens))
+                    orf_antisens_list.append(str(orf_TGA_antisens))
                 if len(str(orf_TAG_antisens)) >= 30 and len(str(orf_TAG_antisens))%3==0:
-                    orf_list.append(str(orf_TAG_antisens))
+                    orf_antisens_list.append(str(orf_TAG_antisens))
                 if len(str(orf_TAA_antisens)) >= 30 and len(str(orf_TAA_antisens))%3==0:
-                    orf_list.append(str(orf_TAA_antisens)) 
+                    orf_antisens_list.append(str(orf_TAA_antisens)) 
             
-            nombre_ORF.append(len(orf_list))
-            orf.append(orf_list)
+            nombre_ORF_sens.append(len(orf_sens_list))
+            nombre_ORF_antisens.append(len(orf_antisens_list))
+            ORF_sens.append(orf_sens_list)
+            ORF_antisens.append(orf_antisens_list)
 
 
 #Creation dataframe
-df = pandas.DataFrame(columns = ['gene', 'mRNA_name','mRNA_sequence','longueur_mRNA','nombre_ORF','ORF_sequences'])
+df = pandas.DataFrame(columns = ['gene','transcript_name','mutation_name','mRNA_sens', 'mRNA_antisens','longueur_mRNA','nombre_ORF_sens','ORF_sens','nombre_ORF_antisens','ORF_antisens'])
 df['gene'] = pandas.Series(gene)
-df['mRNA_name'] = pandas.Series(mRNA_name)
-df['mRNA_sequence'] = pandas.Series(mRNA_sequence)
+df['transcript_name'] = pandas.Series(transcript_name)
+df['mutation_name'] = pandas.Series(mutation_name)
+df['mRNA_sens'] = pandas.Series(mRNA_sens)
+df['mRNA_antisens'] = pandas.Series(mRNA_antisens)
 df['longueur_mRNA'] = pandas.Series(longueur_mRNA)
-df['nombre_ORF'] = pandas.Series(nombre_ORF)
-df['ORF_sequences'] = pandas.Series(orf)
+df['nombre_ORF_sens'] = pandas.Series(nombre_ORF_sens)
+df['nombre_ORF_antisens'] = pandas.Series(nombre_ORF_antisens)
+df['ORF_sens'] = pandas.Series(ORF_sens)
+df['ORF_antisens'] = pandas.Series(ORF_antisens)
 
 #Export vers csv
 pandas.DataFrame.to_csv(df, 'ORF.csv')
