bf09b32ff440ebf3a2febb8b55cff61f1e32f577
juugeebee <july2023@yahoo.fr>
2019-11-19 17:15:51 +0100

    initialisation

---
+++ find_fsATI.py
@@ -0,0 +1,11 @@
+import pandas
+
+#vérifier qu’il existe bien en 3’ une partie de séquence identique à WT
+#chercher l’ins/del qui a rétabli le cadre de lecture
+#déterminer quelle mutation a décalé le site d’initialisation de la traduction en 5’
+
+
+
+### MAIN ##
+df = pandas.read_csv('ORF.csv')
+
